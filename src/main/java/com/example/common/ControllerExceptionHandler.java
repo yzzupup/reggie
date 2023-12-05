1ffa94db8678b1b81d1d0500395627611826738c
yzzupup <yzz372941597@163.com>
2023-12-05 09:59:06 +0800

    修改全局异常处理器

--- src/main/java/com/example/common/ControllerExceptionHandler.java
+++ src/main/java/com/example/common/ControllerExceptionHandler.java
@@ -27,23 +27,31 @@ public class ControllerExceptionHandler {
     @ExceptionHandler({Exception.class})
     public R<String> exceptionHandler(Exception ex){
 
-        // 获取异常堆栈信息
-        StackTraceElement[] stackTrace = ex.getStackTrace();
-
-        // 查找第一个指向您自己代码的元素
-        StackTraceElement firstCustomElement = findFirstCustomElement(stackTrace);
-
-        // 获取异常发生的类名
-        String controllerName = firstCustomElement.getClassName();
-
-        // 获取异常发生的代码行号
-        int lineNumber = firstCustomElement.getLineNumber();
+//        // 获取异常堆栈信息
+//        StackTraceElement[] stackTrace = ex.getStackTrace();
+//
+//        // 查找第一个指向您自己代码的元素
+//        StackTraceElement firstCustomElement = findFirstCustomElement(stackTrace);
+//
+//        // 获取异常发生的类名
+//        String controllerName = firstCustomElement.getClassName();
+//
+//        // 获取异常发生的代码行号
+//        int lineNumber = firstCustomElement.getLineNumber();
+//
+//        log.error(String.format("\n ERROR! %s 发生异常" +
+//                " \n 异常原因: %s" +
+//                "\n 异常类为: %s" +
+//                "\n 异常位置: %s",
+//                LocalDateTime.now(), ex.getMessage(), controllerName, lineNumber));
 
         log.error(String.format("\n ERROR! %s 发生异常" +
                 " \n 异常原因: %s" +
-                "\n 异常类为: %s" +
-                "\n 异常位置: %s",
-                LocalDateTime.now(), ex.getMessage(), controllerName, lineNumber));
+                LocalDateTime.now(), ex.getMessage()));
+
+        String msg = ex.getMessage();
+        if(msg.contains("已存在"))
+            return R.error(msg);
 
         return R.error("出现未知错误");
     }
